df8dacf82fb5f372531cea141125a95faf798966
EIBerlin <>
2026-04-16 14:48:15 +0900

    rust: quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,12 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(c: u8) -> u8 {
+    if c > 40 {
+        return c * 1;
+    }
+    c * 2
+}
 
 fn main() {
     // You can optionally experiment here.
