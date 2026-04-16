c43fb3a4ea55baae5a338cfea80a62d8575c6928
EIBerlin <>
2026-04-16 14:40:55 +0900

    rust: vecs

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -1,9 +1,10 @@
 fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
-    for element in input {
+    for &element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(element * 2);
     }
 
     output
@@ -22,11 +23,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     // vector.
     // See the example in the function `vec_map_example` above.
     input
-        .iter()
-        .map(|element| {
-            // ???
-        })
-        .collect()
+        .iter().map(|&element| element * 2).collect()
 }
 
 fn main() {
